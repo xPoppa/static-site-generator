51560ac3f7d780e299f9f0f6f9607529e2e74470
Romano <lynch@floydhamilton.nl>
2024-03-23 01:31:16 +0100

    Refactored markdown to blocks

--- src/block_markdown_parsing.py
+++ src/block_markdown_parsing.py
@@ -1,16 +1,6 @@
 
 def markdown_to_blocks(markdown: str) -> list[str]:
-    new_lines =  markdown.split("\n")
-    blocks: list[str] = []
-    block = ""
-    for idx, line in enumerate(new_lines):
-        if line != "":
-            block += " " + line
-            if idx == len(new_lines) - 1:
-                blocks.append(block)
-        if line == "":
-            blocks.append(block)
-            block = ""
+    blocks: list[str] =  markdown.split("\n\n")
     for idx, bl in enumerate(blocks):
-        blocks[idx] = bl.strip()
+        blocks[idx] = bl.replace("\n", " ").strip()
     return blocks
