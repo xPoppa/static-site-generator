e78fbe31973be0b8e801a840d255baf4ee6bf7d9
Romano <lynch@floydhamilton.nl>
2024-03-23 21:00:05 +0100

    WIP block markdown to types

--- src/block_markdown_parsing.py
+++ src/block_markdown_parsing.py
@@ -1,6 +1,38 @@
 
+block_type_paragraph    = "paragrapgh"
+block_type_heading      = "heading"
+block_type_code         = "code"
+block_type_quote        = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
 def markdown_to_blocks(markdown: str) -> list[str]:
-    blocks: list[str] =  markdown.split("\n\n")
-    for idx, bl in enumerate(blocks):
-        blocks[idx] = bl.replace("\n", " ").strip()
+    raw_blocks: list[str] =  markdown.split("\n\n")
+    blocks: list[str] = []
+    for block in raw_blocks:
+        if block != "":
+            blocks.append(block.strip().strip("\n"))
     return blocks
+
+
+def block_to_block_type(block: str) -> str:
+    if block[0:2] == "# ":
+        return block_type_heading
+    if block[0:3] == "## ":
+        return block_type_heading
+    if block[0:4] == "### ":
+        return block_type_heading
+    if block[0:5] == "#### ":
+        return block_type_heading
+    if block[0:6] == "##### ":
+        return block_type_heading
+    if block[0:7] == "###### ":
+        return block_type_heading
+    if block[0] == ">":
+        result = []
+        sub_blocks = block.split("\n")
+        for sub_block in sub_blocks:
+            result.append(sub_block[0] == ">")
+        if result.count(True) == len(result) and result:
+            return block_type_quote
+    return block_type_paragraph
--- src/test_block_markdown_parsing.py
+++ src/test_block_markdown_parsing.py
@@ -1,17 +1,79 @@
 import unittest
 
-from block_markdown_parsing import markdown_to_blocks
+from block_markdown_parsing import (
+        block_to_block_type,
+        markdown_to_blocks,
+        block_type_heading,
+        block_type_paragraph
+        )
 
 
 class TestToBlocks(unittest.TestCase):
-    markdown = """This is **bolded** paragraph
+    def test_to_blocks(self):
+
+        markdown = """
+This is **bolded** paragraph
 
 This is another paragraph with *italic* text and `code` here
 This is the same paragraph on a new line
 
-* This is a list
- * with items"""
 
-    print(f"The markdown: \n {markdown}")
-    blocks = markdown_to_blocks(markdown)
-    print(blocks)
+* This is a list
+* with items
+"""
+        blocks = markdown_to_blocks(markdown)
+        expected_blocks = [
+                            "This is **bolded** paragraph",
+                            "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
+                            "* This is a list\n* with items"
+
+
+                          ]
+        self.assertListEqual(blocks, expected_blocks)
+
+    def test_markdown_to_blocks_newlines(self):
+        md = """
+This is **bolded** paragraph
+
+
+
+
+
+
+This is another paragraph with *italic* text and `code` here
+This is the same paragraph on a new line
+
+
+
+* This is a list
+* with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
+                "* This is a list\n* with items",
+            ],
+        )
+
+class TestToBlockTypes(unittest.TestCase):
+    def test_to_block_type_header(self):
+        block_type = block_to_block_type("# miauw")
+        block_type2 = block_to_block_type("## miauw")
+        block_type3 = block_to_block_type("### miauw")
+        block_type4 = block_to_block_type("#### miauw")
+        block_type5 = block_to_block_type("##### miauw")
+        block_type6 = block_to_block_type("###### miauw")
+        block_type_wrong = block_to_block_type("####### miauw")
+        self.assertEqual(block_type, block_type_heading)
+        self.assertEqual(block_type2, block_type_heading)
+        self.assertEqual(block_type3, block_type_heading)
+        self.assertEqual(block_type4, block_type_heading)
+        self.assertEqual(block_type5, block_type_heading)
+        self.assertEqual(block_type6, block_type_heading)
+        self.assertEqual(block_type_wrong, block_type_paragraph)
+
+if __name__ == "__main__":
+    unittest.main()
